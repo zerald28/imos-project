36bf8f4bfed90fa8b53227b7d9fa0d7ec4aaf6c4
jerald <cabusasjerald767@gmail.com>
2026-03-27 22:53:45 +0800

    Add new file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -27,6 +28,14 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\UpdateLastSeen::class, // ← add here
             
         ]);
+
+        $middleware->trustProxies(
+            '*',
+            Request::HEADER_X_FORWARDED_FOR |
+             Request::HEADER_X_FORWARDED_HOST |
+              Request::HEADER_X_FORWARDED_PORT |
+              Request::HEADER_X_FORWARDED_PROTO
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
