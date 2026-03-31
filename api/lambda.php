933e1ea1b5823c908c5ef013c1fea5a2dc59c69e
jerald <cabusasjerald767@gmail.com>
2026-03-31 22:14:46 +0800

    dfdfgfd

--- api/lambda.php
+++ api/lambda.php
@@ -1,15 +1,20 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+define('LARAVEL_START', microtime(true));
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
 
-$response->send();
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-$kernel->terminate($request, $response);
+$app->handleRequest(Request::capture());
